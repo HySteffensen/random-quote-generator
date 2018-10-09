e67d3022d5f039d22006cfb63a758955a7f91613
HySteffensen <hyrum.steffensen@gmail.com>
2018-10-09 10:15:13 -0700

    configured jshint

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,10 @@
 (function() {
     "use strict";
 
+    var linter = require("simplebuild-jshint");
+
     desc("Default");
-    task("default", function() {
+    task("default", [ "lint" ], function() {
         console.log("\n\nBUILD OK");
     });
 
@@ -11,7 +13,45 @@
         jake.exec("node ./node_modules/http-server/bin/http-server ./", { interactive: true }, complete);
     }, { async: true });
 
+    desc("Lint Everything");
+    task("lint", function() {
+        console.log("Linting JavaScript:");
 
+        linter.checkFiles({
+            files: [ "Jakefile.js", "./js/script.js"],
+            options: {
+                bitwise: true,
+                eqeqeq: true,
+                forin: true,
+                freeze: true,
+                futurehostile: true,
+                latedef: "nofunc",
+                noarg: true,
+                nocomma: true,
+                nonbsp: true,
+                nonew: true,
+                strict: true,
+                undef: true,
+    
+                node: true,
+                browser: true
+            },
+            globals: {
+                complete: false,
+                describe: false,
+                desc: false,
+                task: false,
+                fail: false,
+                it: false,
+                before: false,
+                after: false,
+                beforeEach: false,
+                afterEach: false,
+                jake: false,
+                directory: false
+            }
+        }, complete, fail);
+    }, { async: true });
 
 
 
