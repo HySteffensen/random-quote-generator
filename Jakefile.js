bb1b3559a38f8297da5e4d77dae521d89d4d6d15
HySteffensen <hyrum.steffensen@gmail.com>
2018-10-08 20:27:07 -0700

    configured localhost webserver

--- Jakefile.js
+++ Jakefile.js
@@ -1,12 +1,17 @@
 (function() {
     "use strict";
 
-    desc("default");
+    desc("Default");
     task("default", function() {
         console.log("\n\nBUILD OK");
     });
 
-    
+    desc("Run localhost server");
+    task("run", function() {
+        jake.exec("node ./node_modules/http-server/bin/http-server ./", { interactive: true }, complete);
+    }, { async: true });
+
+
 
 
 
