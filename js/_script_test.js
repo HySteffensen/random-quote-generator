78ec7edb4147091fd39e615ff6d45e576fba8fd1
HySteffensen <hyrum.steffensen@gmail.com>
2018-10-09 19:22:02 -0700

    made failing test for mocha

---
+++ js/_script_test.js
@@ -0,0 +1,25 @@
+(function() {
+    "use strict";
+
+    var assert = require("../vendor/chai-v4.1.2").assert;
+
+    describe("TEST", function() {
+        it("makes a test", function() {
+            assert.equal(true, false);
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}());
